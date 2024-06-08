e34c642dfdbcffbe10a8a171dedeeac805ed7d5f
pimgmp <pimgmp@gmail.com>
2024-06-08 14:39:37 -0500

    TODO App finalization

--- todo_main/views.py
+++ todo_main/views.py
@@ -3,7 +3,11 @@ from todo.models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by('updated_at') #filter para mas de un dato
+    
+    completed_task = Task.objects.filter(is_completed=True)
+    
     context = {
         'tasks': tasks,
+        'completed_task': completed_task,
     }
     return render(request, 'home.html', context)
